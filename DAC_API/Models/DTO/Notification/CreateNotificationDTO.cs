936bc09110ed26696a9ce323f947a33f3be59fe2
Domagoj Hegedušić <dhegedusi21@foi.hr>
2025-05-06 21:41:55 +0200

    Created Notification methods.

---
+++ DAC_API/Models/DTO/Notification/CreateNotificationDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAC_API.Models.DTO.Notification {
+    public class CreateNotificationDTO {
+        [Required(ErrorMessage = "User ID is required")]
+        public int UserId { get; set; }
+
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        public string Message { get; set; }
+
+        public string? Type { get; set; }
+    }
+}
